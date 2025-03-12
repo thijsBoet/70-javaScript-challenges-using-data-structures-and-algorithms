b6205a75ac522da254e05cbade4ba68bd2d5064b
Matthijs Boet <m.boet@chello.nl>
2025-03-12 14:37:09 +0100

    finished basic-problems 1

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,17 @@
-function removeDuplicates() {}
+function removeDuplicates(array) {
+  const uniqueArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (!uniqueArray.includes(array[i])) {
+      uniqueArray.push(array[i]);
+    }
+  }
+
+  return uniqueArray;
+  
+  return [...new Set(array)];
+
+  return Array.from(new Set(array));
+}
 
 module.exports = removeDuplicates;
