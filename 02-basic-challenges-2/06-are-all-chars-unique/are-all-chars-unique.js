d27901832d3bfc722924ecdd27f950d1dea46fd1
Matthijs Boet <m.boet@chello.nl>
2025-03-18 16:48:41 +0100

    finsihned 2.6

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,24 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(string) {
+    // return string === [...new Set(string)].join('');
+
+    // const charCount = {};
+    // for ( const char of string) {
+    //     if (charCount[char]) {
+    //         return false;
+    //     } 
+    //     charCount[char] = true;
+    // }
+    // return true;
+
+    const charSet = new Set();
+
+    for (const char of string) {
+        if (charSet.has(char)) {
+            return false;
+        }
+        charSet.add(char);
+    }
+    return true;
+}
 
 module.exports = areAllCharactersUnique;
