9516b86928226a76039f043934f11b96f736f3e5
Matthijs Boet <m.boet@chello.nl>
2025-03-26 13:47:25 +0100

    half section 3

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,29 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  const charMap1 = str1.split('').reduce((acc, char) => {
+    acc[char] = acc[char] + 1 || 1;
+    return acc;
+  }, {});
+
+  const charMap2 = str2.split('').reduce((acc, char) => {
+		acc[char] = acc[char] + 1 || 1;
+		return acc;
+  }, {});
+
+  return (
+		Object.keys(charMap1).every(
+			char => charMap1[char] === charMap2[char],
+		) &&
+		Object.keys(charMap2).every(char => charMap1[char] === charMap2[char])
+  );
+}
 
 module.exports = validAnagrams;
+
+
+
+
+
+
+
+
+
